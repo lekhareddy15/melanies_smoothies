b72bb003790bd8537719bc51c03a964df147d1c4
Lekha Reddy Surakanti <113005394+lekhareddy15@users.noreply.github.com>
2026-06-13 18:41:54 -0400

    Add Snowflake connection and session setup
    Establish Snowflake connection and session in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -40,4 +39,7 @@ if ingredients_list:
  if time_to_insert:
      session.sql(my_insert_stmt).collect()
      st.success('Your Smoothie is ordered!', icon="✅")
+
+cnx = st.connection("snowflake")
+session = cnx.session()
     
