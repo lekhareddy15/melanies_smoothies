28f0fd8d8fcbacbd9338c0a84b529234f80a3708
Lekha Reddy Surakanti <113005394+lekhareddy15@users.noreply.github.com>
2026-06-13 19:14:01 -0400

    Comment out Snowflake session code
    Comment out the import of get_active_session and its usage.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -15,7 +15,7 @@ st.write(
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
 
-session = get_active_session()
+#session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
