eab6877fb0ca80be5c308cd89e2913346c21d674
Lekha Reddy Surakanti <113005394+lekhareddy15@users.noreply.github.com>
2026-06-13 21:18:59 -0400

    Fetch and display watermelon data in Streamlit app
    Added a request to fetch watermelon data and display it in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
+
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -31,12 +33,16 @@ if ingredients_list:
  ingredients_string = ''
  for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
  #st.write(ingredients_string)
  name_on_order = st.text_input("Name on Smoothie")
  my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
 
-#st.write(my_insert_stmt)
+ #st.write(my_insert_stmt)
  time_to_insert = st.button('Submit Order')
 
 
@@ -45,9 +51,5 @@ if ingredients_list:
      st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ 
     
